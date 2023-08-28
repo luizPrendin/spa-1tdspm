ee107eddea57dbbc7e5e0b4b2fb517f985490cc2
luizPrendin <luizprendinfelipe@gmail.com>
2023-08-28 17:41:52 -0300

    iniciando componentes

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import cabecalho from"./components/cabecalho"
+
+export default function App(){
+  
+
+  return(
+    <>
+      <div className="container">
+        {/*SINTAXE COMMENT*/}
+        {/*Vamos criar um cabecalho com header, um h1 com titulo
+        e uma lista ul com 3 item com link*/}
+
+        <header className="cabecalho">
+          <h1>Vite  +  React</h1>
+          
+          <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+          </ul>
+        
+        </header>
+
+        <section>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet natus nobis itaque quis eaque similique tempora rem sit voluptatibus harum minus, quo soluta consequatur quod dolores nihil numquam facere ducimus maxime ratione vero molestias, error incidunt. Quisquam, laudantium maiores! Voluptatum exercitationem reiciendis ullam, sapiente saepe dignissimos voluptate hic amet quisquam nulla vitae? Nihil recusandae itaque amet dignissimos omnis vel temporibus! Beatae perferendis labore, fugit repellat quo temporibus totam aliquid autem eos eveniet repudiandae qui dolor culpa sunt ullam tempore repellendus molestiae ipsa inventore pariatur iure dolorem ex. Voluptatum, ipsum rem natus recusandae modi et perferendis nisi architecto enim dolorem? Aperiam.</p>
+            <p>Possimus incidunt aliquam consectetur, officiis voluptas natus, beatae rerum aliquid necessitatibus voluptatum explicabo eveniet atque cum saepe a nobis. Ab nisi illum est eaque suscipit eum asperiores aut, illo voluptate porro, amet doloremque similique expedita, ex quis. Facilis cum asperiores quisquam ut commodi alias omnis corrupti in cumque amet? Provident quisquam omnis odit quaerat, laborum debitis nihil voluptatum maiores, expedita molestiae est ratione illo impedit, et nisi voluptas iste doloremque nam distinctio. Quibusdam, tempore. Labore at quod, architecto iste perferendis sunt incidunt ducimus, provident voluptas nemo non doloremque et, adipisci ex quis officiis a impedit. Ab natus blanditiis libero id?</p>
+            <p>Quae debitis possimus amet veniam sed doloribus, officia necessitatibus eligendi aspernatur expedita quibusdam fugit facere corrupti quia quis consectetur dignissimos. Reiciendis odit ipsa repellendus mollitia amet sapiente qui. Voluptate quaerat architecto, ea tempore odio, accusantium porro nam ab ipsa quibusdam temporibus, facilis vero iure quas accusamus iste itaque animi atque asperiores dolore necessitatibus. Porro facere adipisci eveniet quos laborum ratione nobis pariatur laboriosam libero voluptatibus, nihil similique tenetur officiis explicabo neque eos necessitatibus aut sint quo dignissimos. Voluptatum animi maxime voluptates earum aut molestias quidem sapiente omnis! Odit aperiam harum nisi, magnam asperiores, incidunt at repellendus deserunt nam commodi aliquid.</p>
+          <img src="" alt="" />
+          </div>
+        </section>
+        
+        <footer>
+          <div>
+            <ul>
+              <li><a href="">Git Hub</a></li>
+              <li><a href="">X</a></li>
+              <li><a href="">Reddit</a></li>
+
+              <p>%copy Todos os direitos reservados</p>
+            </ul>
+          </div>
+        </footer>
+
+
+
+
+      </div>
+    </>
+  )
+}
