3b271129d512643632b9e777801476d5939bae5f
luizPrendin <luizprendinfelipe@gmail.com>
2023-09-04 12:57:49 -0300

    Lista dos dados, Recuperação do id via hook

--- src/components/cabecalho.jsx
+++ src/components/cabecalho.jsx
@@ -13,15 +13,7 @@ export default function cabecalho() {
             <li>
                 <Link to="/produtos">Produtos</Link>
             </li>
-            <li>
-                <Link to="/editar/produtos/1">Produto - 1</Link>
-            </li>
-            <li>
-                <Link to="/editar/produtos/2">Produto - 2</Link>
-            </li>
-            <li>
-                <Link to="/editar/produtos/3">Produto - 3</Link>
-            </li>
+            
           </ul>
         
         </header>
