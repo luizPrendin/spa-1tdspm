b38882633a7ab44a2efb85a46217f5f7e260b051
luizPrendin <luizprendinfelipe@gmail.com>
2023-09-04 13:07:54 -0300

    recuperando objeto no forms

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -15,7 +15,27 @@ export default function EditarProdutos() {
       <h1>
         Editar - Produtos
       </h1>
-      <p>Produto Selecionado -{produtoRecuperadoDaListaById[0].nome}</p>
-    </div>
+
+      <div>
+          <form>
+            <fieldset>
+              <legend>PRODUTO SELECIONADO</legend>
+              <div>
+                <label htmlFor="idNome">Nome do Produto:</label>
+                <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperadoDaListaById[0].nome}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição do Produto:</label>
+                <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperadoDaListaById[0].desc}/>
+              </div>
+              <div>
+                <label htmlFor="idValor">Valor do Produto:</label>
+                <input type="text" name="valor" id="idValor" defaultValue={produtoRecuperadoDaListaById[0].valor}/>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+
+   </div>
   )
 }
