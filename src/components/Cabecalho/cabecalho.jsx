3d7ac41ab376f622feb3e259f5b1248134a761a7
luizPrendin <luizprendinfelipe@gmail.com>
2023-10-06 09:53:49 -0300

    utilizando useLocation

--- src/components/Cabecalho/cabecalho.jsx
+++ src/components/Cabecalho/cabecalho.jsx
@@ -1,22 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import{AiOutlineHome as H} from "react-icons/ai"
 import "./Cabecalho.scss"
 
 
 export default function Cabecalho() {
+
+  const rotaAtual = useLocation()
+
+  console.log("Rota atual: " +rotaAtual.pathname)
+
   return (
     <header className="cabecalho">
           
           
           <ul>
             <li>
-                <Link to="/">Home - <H/></Link>
+                <Link to="/" className={rotaAtual.pathname =="/" ? "active":""}>Home - <H/></Link>
             </li>
             <li>
-                <Link to="/produtos">Produtos</Link>
+                <Link to="/produtos" className={rotaAtual.pathname =="/produtos" ? "active":""}>Produtos</Link>
             </li>
             <li>
-                <Link to="/cadastrar/produto">Cadastrar Produtos</Link>
+                <Link to="/cadastrar/produto" className={rotaAtual.pathname =="/cadastrar/produto" ? "active":""}>Cadastrar Produtos</Link>
             </li>
             
           </ul>
